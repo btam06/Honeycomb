c6e341161cb6e43ca497b8c9ae591c14515a60ee
Avery Tam <averytam@imarc.com>
2022-04-04 18:57:03 -0700

    misc fixes to array flatten

--- src/functions/ArrayFlatten.php
+++ src/functions/ArrayFlatten.php
@@ -5,12 +5,14 @@ if (!function_exists('array_flatten')) {
     function array_flatten($array, $namespace = '') {
         $output = [];
         foreach ($array as $key => $value) {
+            $key = !empty($namespace) ? $namespace . '.' . $key : $key;
             if (is_array($value)) {
                 $output = $output + array_flatten($value, $key);
             } else {
-                $key = !empty($namespace) ? $namespace . '.' . $key : $key;
                 $output[$key] = $value;
             }
         }
+
+        return $output;
     }
 }
